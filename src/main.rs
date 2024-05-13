67efaf8c8b740898e45d6683c311d1fb286c8f5c
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2024-05-14 00:16:50 +0900

    feat: Implement unit cube

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,100 @@
+const SOURCE_CODE: &str = "cube()";
+
 fn main() {
-    println!("Hello, world!");
+    if SOURCE_CODE == "cube()" {
+        // Z-axis Back
+        println!("solid made_by_orthoptera");
+        println!("facet normal 0.0 0.0 -1.0");
+        println!("  outer loop");
+        println!("    vertex 0.0 0.0 0.0");
+        println!("    vertex 1.0 1.0 0.0");
+        println!("    vertex 1.0 0.0 0.0");
+        println!("  endloop");
+        println!("endfacet");
+        println!("facet normal 0.0 0.0 -1.0");
+        println!("  outer loop");
+        println!("    vertex 0.0 1.0 0.0");
+        println!("    vertex 1.0 1.0 0.0");
+        println!("    vertex 0.0 0.0 0.0");
+        println!("  endloop");
+        println!("endfacet");
+        // Z-axis Front
+        println!("facet normal 0.0 0.0 1.0");
+        println!("  outer loop");
+        println!("    vertex 0.0 0.0 1.0");
+        println!("    vertex 1.0 0.0 1.0");
+        println!("    vertex 1.0 1.0 1.0");
+        println!("  endloop");
+        println!("endfacet");
+        println!("facet normal 0.0 0.0 1.0");
+        println!("  outer loop");
+        println!("    vertex 0.0 0.0 1.0");
+        println!("    vertex 1.0 1.0 1.0");
+        println!("    vertex 0.0 1.0 1.0");
+        println!("  endloop");
+        println!("endfacet");
+        // Y-axis Bottom
+        println!("facet normal 0.0 -1.0 0.0");
+        println!("  outer loop");
+        println!("    vertex 0.0 0.0 0.0");
+        println!("    vertex 1.0 0.0 0.0");
+        println!("    vertex 1.0 0.0 1.0");
+        println!("  endloop");
+        println!("endfacet");
+        println!("facet normal 0.0 -1.0 0.0");
+        println!("  outer loop");
+        println!("    vertex 0.0 0.0 0.0");
+        println!("    vertex 1.0 0.0 1.0");
+        println!("    vertex 0.0 0.0 1.0");
+        println!("  endloop");
+        println!("endfacet");
+        // Y-axis Top
+        println!("facet normal 0.0 1.0 0.0");
+        println!("  outer loop");
+        println!("    vertex 1.0 1.0 1.0");
+        println!("    vertex 1.0 1.0 0.0");
+        println!("    vertex 0.0 1.0 0.0");
+        println!("  endloop");
+        println!("endfacet");
+        println!("facet normal 0.0 1.0 0.0");
+        println!("  outer loop");
+        println!("    vertex 0.0 1.0 1.0");
+        println!("    vertex 1.0 1.0 1.0");
+        println!("    vertex 0.0 1.0 0.0");
+        println!("  endloop");
+        println!("endfacet");
+        // X-axis Back
+        println!("facet normal -1.0 0.0 0.0");
+        println!("  outer loop");
+        println!("    vertex 0.0 1.0 1.0");
+        println!("    vertex 0.0 1.0 0.0");
+        println!("    vertex 0.0 0.0 0.0");
+        println!("  endloop");
+        println!("endfacet");
+        println!("facet normal -1.0 0.0 0.0");
+        println!("  outer loop");
+        println!("    vertex 0.0 0.0 1.0");
+        println!("    vertex 0.0 1.0 1.0");
+        println!("    vertex 0.0 0.0 0.0");
+        println!("  endloop");
+        println!("endfacet");
+        // X-axis Front
+        println!("facet normal 1.0 0.0 0.0");
+        println!("  outer loop");
+        println!("    vertex 1.0 0.0 0.0");
+        println!("    vertex 1.0 1.0 0.0");
+        println!("    vertex 1.0 1.0 1.0");
+        println!("  endloop");
+        println!("endfacet");
+        println!("facet normal 1.0 0.0 0.0");
+        println!("  outer loop");
+        println!("    vertex 1.0 0.0 0.0");
+        println!("    vertex 1.0 1.0 1.0");
+        println!("    vertex 1.0 0.0 1.0");
+        println!("  endloop");
+        println!("endfacet");
+        println!("endsolid made_by_orthoptera");
+    } else {
+        panic!("COMPILE ERROR");
+    }
 }
